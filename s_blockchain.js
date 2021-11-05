f9f5d1b6c43e1ce2ed9ca4350f8cb2986e1baced
furky.eth <52575351+furkananter@users.noreply.github.com>
2021-11-06 01:05:29 +0300

    Update s_blockchain.js

--- s_blockchain.js
+++ s_blockchain.js
@@ -1,1 +1,37 @@
 const crypto = require('crypto');
+class blockchain {
+    constructor() {
+        if (!blockchain.instance) {
+            this.pendingTranscation = [];
+            this.chain = [];
+            blockchain.instance = this;
+        }
+        return blockchain.instance;
+    }
+    createGenesisBlock(block) {
+        this.chain.push(block);
+    }
+    addBlocktoChain(block) {
+        if (this.isBlockValid()) {
+            this.chain.push(block);
+            this.pendingTranscation = [];
+        }
+    }
+    isBlockValid() {
+        return block.hash === block.createHash();
+    }
+}
+class block {
+    constructor(timestamp, transaction, difficulty, previousBlockHash = '') {
+        this.timestamp = timestamp;
+        this.nonce = 0;
+        this.transaction = transaction;
+        this.difficulty = difficulty;
+        this.previousBlockHash = previousBlockHash;
+        this.hash = 0;
+    }
+
+    createHash() {
+
+    }
+}
